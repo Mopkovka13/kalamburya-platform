37850500965c8762b462c0a297042011fac4801f
mopkovka13 <evgeniyneskin13@gmail.com>
2026-02-23 14:27:24 +0300

    Пень

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -4,6 +4,25 @@ plugins {
     id("java")
 }
 
+group = "ru.mopkovka"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.spring.cloud.get()}")
+    }
+}
+
 dependencies {
     implementation(libs.spring.cloud.eureka.server)
     implementation(libs.spring.boot.starter.actuator)
